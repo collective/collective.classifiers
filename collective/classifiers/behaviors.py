441691c8feeed5993c7a728960a2c090c1ce5f2d
Fred van Dijk <fredvd@gmail.com>
2014-04-07 12:17:49 +0200

    Add descriptions to our theme and classifier Provide Dutch translations

--- collective/classifiers/behaviors.py
+++ collective/classifiers/behaviors.py
@@ -10,6 +10,10 @@ class IThemes(model.Schema):
 
     classifiers_themes = schema.List(
         title=_(u'Themes'),
+        description=_(u"Select one or more subjects that match this objects "
+                       "content to classify it for search in other parts of "
+                       "the site. Themes are specific to a website's "
+                       "purpose."),
         value_type=schema.Choice(
             vocabulary='collective.classifiers.themes'),
         required=False,
@@ -21,6 +25,12 @@ class ICategories(model.Schema):
 
     classifiers_categories = schema.List(
         title=_(u'Categories'),
+        description=_(u"The category classifies the meta type of information this "
+                       "content contains and could be seen as different "
+                       "sections on the site, for example Q&A, legal, or "
+                       "support pages. Categories are more generic and "
+                       "can be found under the same name (like support) "
+                       "on many different websites."),
         value_type=schema.Choice(
             vocabulary='collective.classifiers.categories'),
         required=False,
